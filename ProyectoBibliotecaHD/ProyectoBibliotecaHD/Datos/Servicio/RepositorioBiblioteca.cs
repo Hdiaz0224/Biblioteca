2b9b2c5ca52ccc6e36caf979d385e93dd4623353
Hdiaz0224 <93561095+Hdiaz0224@users.noreply.github.com>
2022-04-28 18:50:50 -0500

    Proyecto

---
+++ ProyectoBibliotecaHD/ProyectoBibliotecaHD/Datos/Servicio/RepositorioBiblioteca.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoBibliotecaHD.Datos.modelo;
+
+namespace ProyectoBibliotecaHD.Datos.Servicio
+{
+    public class RepositorioBiblioteca : DbContext
+
+    {
+        public RepositorioBiblioteca(DbContextOptions<RepositorioBiblioteca>options):base(options)
+        {
+
+        }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Autor> Autor { get; set; }
+        public DbSet<Estudiante> Estudiante { get; set; }
+        public DbSet<Prestamo> Prestamo { get; set; }
+
+    }
+}
